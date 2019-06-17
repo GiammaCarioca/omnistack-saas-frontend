656093f76c277d74efdf9a87250fb317deb7ef8d
Thiago Giammattey <giammattey@gmail.com>
2019-06-17 12:04:28 -0300

    adding modal: Create Team

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -5,12 +5,19 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import TeamsActions from '~/store/ducks/teams';
 
-import { Container, TeamList, Team } from './styles';
+import Button from '~/styles/components/Button';
+import Modal from '~/components/Modal';
+
+import {
+  Container, TeamList, Team, NewTeam,
+} from './styles';
 
 class TeamSwitcher extends Component {
   static propTypes = {
     getTeamsRequest: PropTypes.func.isRequired,
     selectTeam: PropTypes.func.isRequired,
+    openTeamModal: PropTypes.func.isRequired,
+    closeTeamModal: PropTypes.func.isRequired,
     teams: PropTypes.shape({
       data: PropTypes.arrayOf(
         PropTypes.shape({
@@ -34,7 +41,7 @@ class TeamSwitcher extends Component {
   };
 
   render() {
-    const { teams } = this.props;
+    const { teams, openTeamModal, closeTeamModal } = this.props;
 
     return (
       <Container>
@@ -48,6 +55,25 @@ class TeamSwitcher extends Component {
             </Team>
           ))}
         </TeamList>
+        <NewTeam onClick={openTeamModal}>NOVO</NewTeam>
+
+        { teams.teamModalOpen && (
+          <Modal>
+            <h1>Criar Time</h1>
+
+            <form onSubmit={() => {}}>
+              <span>NOME</span>
+              <input name="newTeam" />
+
+              <Button type="submit" size="big">
+                Salvar
+              </Button>
+              <Button onClick={closeTeamModal} size="small" color="gray">
+                Cancelar
+              </Button>
+            </form>
+          </Modal>
+        )}
       </Container>
     );
   }
